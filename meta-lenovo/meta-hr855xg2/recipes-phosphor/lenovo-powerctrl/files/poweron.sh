09b31cb6ac506355c91b5b6f31de1a5187fe80fa
Payne Yang <pyang4@lenovo.com>
2019-07-18 19:25:12 +0800

    Add workaround for ipmi chassis power on failed issue Summary: 1. Add service stop op-wait-power-on@0.service in poweron.sh
    Test Plan:
    Build and test pass on hr855xg2
    
    Signed-off-by: Payne Yang <pyang4@lenovo.com>

--- meta-lenovo/meta-hr855xg2/recipes-phosphor/lenovo-powerctrl/files/poweron.sh
+++ meta-lenovo/meta-hr855xg2/recipes-phosphor/lenovo-powerctrl/files/poweron.sh
@@ -14,4 +14,9 @@ sleep 1
 echo 0 > /sys/class/gpio/gpio${GPIO_NUM}/value
 fi
 
+# It is a workaround for power on fail.
+# The root cause is that op-wait-power-on could not detect correct PWR state.
+# We should remove this workaround after we fix the incorrect PWR state issue.
+systemctl stop op-wait-power-on@0.service
+
 exit 0;
