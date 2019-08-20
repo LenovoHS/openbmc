ef41b61cf0e0045b067d1907125078b0e21fc3c3
dlin23 <dlin23@lenovo.com>
2019-08-20 11:42:10 +0800

    hr855xg2: Modify how to check power status for fan control Use DBUS to check power status instead of GPIO pins.
    Signed-off-by: Derek Lin <dlin23@lenovo.com>

--- meta-lenovo/meta-hr855xg2/recipes-phosphor/fans/phosphor-pid-control/start_phosphor-pid-control.sh
+++ meta-lenovo/meta-hr855xg2/recipes-phosphor/fans/phosphor-pid-control/start_phosphor-pid-control.sh
@@ -5,29 +5,21 @@
 
 # phosphor-pid-control flow
 
-GPIO_BASE=$(cat /sys/class/gpio/gpio*/base)
-PWGOOD=$(($GPIO_BASE+63))
-PWGOOD_PATH="/sys/class/gpio/gpio"${PWGOOD}"/value"
 PID_CONTROL="/usr/bin/swampd -l /tmp/"
 
 
 # Check if system is power on or not, if not, then sleep 
 # and wait for power on completion and start PID_CONTROL daemon. 
-if [[ -f ${PWGOOD_PATH} ]] ; then
-    while [ 1 ] ; do
-        st=`cat ${PWGOOD_PATH}`
-        if [ "$st" == "1" ]; then
-            # Wait here until BIOS is ready, so the sensors are available on DBUS.
-            # TODO: query current BIOS status, if it's ready, then start pid-control.
-            sleep 10
-            ${PID_CONTROL}
-        else
-            echo "System is not power on, wait for power-on"
-        fi
-        sleep 1
-    done
-else
-    echo "PWGOOD PIN, ${PWGOOD_PATH} is not defined!!" 
-    exit 1
-fi
- 
+while [ 1 ] ; do
+    status=`busctl get-property org.openbmc.control.Power /org/openbmc/control/power0 org.openbmc.control.Power pgood | awk '{print $2}'`
+
+    if [ $status == "1" ]; then
+        # Wait here until BIOS is ready, so the sensors are available on DBUS.
+        # TODO: query current BIOS status, if it's ready, then start pid-control.
+        sleep 10
+        ${PID_CONTROL}
+    else
+        echo "System is not power on, wait for power-on"
+    fi
+    sleep 1
+done
