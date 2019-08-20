0a56afe4ab3731342ca5e6230a69d17fe36b93c6
Harry Sung <hsung1@lenovo.com>
2019-08-20 15:41:58 +0800

    hr650x+: Add workaround for ipmi chassis power on failed issue
    Summary:
    Add service stop op-wait-power-on@0.service in poweron.sh
    
    Test Plan:
    HR650X+ system can power on successfully.
    
    Signed-off-by: Harry Sung <hsung1@lenovo.com>

--- meta-lenovo/meta-hr650x+/recipes-phosphor/lenovo-powerctrl/files/poweron.sh
+++ meta-lenovo/meta-hr650x+/recipes-phosphor/lenovo-powerctrl/files/poweron.sh
@@ -1,5 +1,5 @@
-#"Copyright ? 2019-present Lenovo
-#Licensed under BSD-3, see COPYING.BSD file for details."
+# Copyright ? 2019-present Lenovo
+# Licensed under BSD-3, see COPYING.BSD file for details.
 
 #!/bin/bash
 
@@ -9,5 +9,10 @@ GPIO_NUM=$(($GPIO_BASE + 64 + 0))
 echo 1 > /sys/class/gpio/gpio${GPIO_NUM}/value
 sleep 1
 echo 0 > /sys/class/gpio/gpio${GPIO_NUM}/value
+
+# It is a workaround for power on fail.
+# The root cause is that op-wait-power-on could not detect correct PWR state.
+# We should remove this workaround after we fix the incorrect PWR state issue.
+systemctl stop op-wait-power-on@0.service
 
 exit 0;
