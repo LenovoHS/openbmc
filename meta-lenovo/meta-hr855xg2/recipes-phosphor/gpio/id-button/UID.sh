c288eabb05322bf3a5f2fd30758bc3ea0e56b33b
Lisa Liu <liuyj19@lenovo.com>
2019-09-29 16:58:51 +0800

    Modify the method to read and write fpga register using dbus, because it has remove fpga tool

--- meta-lenovo/meta-hr855xg2/recipes-phosphor/gpio/id-button/UID.sh
+++ meta-lenovo/meta-hr855xg2/recipes-phosphor/gpio/id-button/UID.sh
@@ -4,13 +4,22 @@
 # Licensed under BSD-3, see COPYING.BSD file for details."
 
 # Get current state
+
+SERVICE="org.openbmc.control.fpga"
+OBJECT="/org/openbmc/control/fpga"
+INTERFACE="org.openbmc.control.fpga"
+READ_METHOD="read_fpga"
+WRITE_METHOD="write_fpga"
+
 uid_on()
 {
-    write_cmd=`fpga -w -b 13 -o 14 -l 1 2`
+    #write_cmd=`fpga -w -b 13 -o 14 -l 1 2`
+    write_cmd=$(busctl call $SERVICE $OBJECT $INTERFACE $WRITE_METHOD yyy 13 14 2)
 }
 uid_off()
 {
-    write_cmd=`fpga -w -b 13 -o 14 -l 1 0`
+    #write_cmd=`fpga -w -b 13 -o 14 -l 1 0`
+    write_cmd=$(busctl call $SERVICE $OBJECT $INTERFACE $WRITE_METHOD yyy 13 14 0)
 }
 
 #read_cmd=`fpga -r -b 13 -o 14 -l 1 |grep receive`
--- meta-lenovo/meta-hr855xg2/recipes-phosphor/gpio/id-button/toggle_identify_led.sh
+++ meta-lenovo/meta-hr855xg2/recipes-phosphor/gpio/id-button/toggle_identify_led.sh
@@ -4,17 +4,26 @@
 # Licensed under BSD-3, see COPYING.BSD file for details."
 
 # Get current state
+SERVICE="org.openbmc.control.fpga"
+OBJECT="/org/openbmc/control/fpga"
+INTERFACE="org.openbmc.control.fpga"
+READ_METHOD="read_fpga"
+WRITE_METHOD="write_fpga"
+
 uid_on() 
 {
-    write_cmd=`fpga -w -b 13 -o 14 -l 1 2` 
+    #write_cmd=`fpga -w -b 13 -o 14 -l 1 2` 
+    write_cmd=$(busctl call $SERVICE $OBJECT $INTERFACE $WRITE_METHOD yyy 13 14 2)
 }
 uid_off()
 {
-    write_cmd=`fpga -w -b 13 -o 14 -l 1 0`
+    #write_cmd=`fpga -w -b 13 -o 14 -l 1 0`
+    write_cmd=$(busctl call $SERVICE $OBJECT $INTERFACE $WRITE_METHOD yyy 13 14 0)
 }
 
-read_cmd=`fpga -r -b 13 -o 14 -l 1 |grep receive`
-read_cmd=${read_cmd#*:}
+#read_cmd=`fpga -r -b 13 -o 14 -l 1 |grep receive`
+#read_cmd=${read_cmd#*:}
+read_cmd=$(busctl call $SERVICE $OBJECT $INTERFACE $READ_METHOD yy 13 14 | awk '{print $2}')
 
 if [ $read_cmd -eq 0 ]; then
     uid_on
