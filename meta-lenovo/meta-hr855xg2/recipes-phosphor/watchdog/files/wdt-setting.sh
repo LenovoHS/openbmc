10daa1bbb0e4ea0fe173c53e9e588397eda1f41e
Harry Sung <hsung1@lenovo.com>
2019-10-21 16:19:30 +0800

    HR850XG2: Fixed wrong WDT2 settings issue
    Root Cause:
    The specific settings for HR855XG2 may be overwritten by default settings
    because the watchdog service have not complete yet.
    
    Fixed:
    Check the watchdog PID file is create or not. If not, wait watchdog service
    finished.
    
    Test Plan:
    Check corresponding register settings are correct
      root@hr855xg2:~# devmem 0x1e78502c
      0x0000001B
      root@hr855xg2:~# devmem 0x1e78503c
      0x021FFFF3
    
    Signed-off-by: Harry Sung <hsung1@lenovo.com>

--- meta-lenovo/meta-hr855xg2/recipes-phosphor/watchdog/files/wdt-setting.sh
+++ meta-lenovo/meta-hr855xg2/recipes-phosphor/watchdog/files/wdt-setting.sh
@@ -6,15 +6,17 @@
 # Make sure watchdog service is ready to avoid script setting be written
 while [ ! -f /var/run/watchdog.pid ]
 do
-  mknod -m 660 /dev/mem c 1 1
-
-  # Enable WDT and sent EXTRST after timeout
-  devmem 0x1E78502C 32 0x1B
-
-  # Disable reset GPIO controller
-  devmem 0x1e78503C 32 0x021FFFF3
-
-  unlink /dev/mem
+  sleep 1
 done
 
+# TODO: /dev/mem will be disabled in the future
+mknod -m 660 /dev/mem c 1 1
+
+# Enable WDT and sent EXTRST after timeout
+devmem 0x1E78502C 32 0x1B
+
+# Disable reset GPIO controller
+devmem 0x1e78503C 32 0x021FFFF3
+
+unlink /dev/mem
 
